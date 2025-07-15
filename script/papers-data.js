de192ef891fa3b9b7243e5efa262569a7d189b99
abellaval <abel.laval@ulb.be>
2025-07-15 12:45:45 +0200

    Added best paper award crypto

--- script/papers-data.js
+++ script/papers-data.js
@@ -3,7 +3,7 @@ const PAPERS_DATA = [
       titre: "KLPT²: Algebraic pathfinding in dimension two and applications",
       auteurs: "Wouter Castryck, Thomas Decru, Péter Kutas, Abel Laval, Christophe Petit, Yan Bo Ti",
       annee: "2025",
-      acceptedat: "CRYPTO 2025",
+      acceptedat: "CRYPTO 2025 – Best paper award",
       
       liens: {
          "pdf": "https://eprint.iacr.org/2025/372.pdf",
