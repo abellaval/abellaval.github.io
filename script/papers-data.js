bc85eef9bdce1dc91d2c45d0f4dc27017d0374a4
abellaval <91144521+abellaval@users.noreply.github.com>
2025-07-25 14:56:57 +0200

    Added hyperlinks

--- script/papers-data.js
+++ script/papers-data.js
@@ -3,7 +3,7 @@ const PAPERS_DATA = [
       titre: "KLPT²: Algebraic pathfinding in dimension two and applications",
       auteurs: "Wouter Castryck, Thomas Decru, Péter Kutas, Abel Laval, Christophe Petit, Yan Bo Ti",
       annee: "2025",
-      acceptedat: "CRYPTO 2025 – Best paper award",
+      acceptedat: "<a class='colored' href='https://crypto.iacr.org/2025'>CRYPTO 2025</a> – Best paper award",
       
       liens: {
          "pdf": "https://eprint.iacr.org/2025/372.pdf",
@@ -14,7 +14,7 @@ const PAPERS_DATA = [
       titre: "Exploring SIDH-based signature parameters",
       auteurs: "Andrea Basso, Mingjie Chen, Tako Boris Fouotsa, Péter Kutas, Abel Laval, Laurane Marco, Gustatve Tchoffo Saah",
       annee: "2023",
-      acceptedat: "ACNS 2024",
+      acceptedat: "<a class='colored' href='https://wp.nyu.edu/acns2024/'>ACNS 2024</a>",
       
       liens: {
          "pdf": "https://eprint.iacr.org/2023/1906.pdf",
@@ -25,7 +25,7 @@ const PAPERS_DATA = [
       titre: "Malleable commitments from group actions and zero-knowledge proofs for circuits based on isogenies",
       auteurs: "Mingjie Chen, Yi-Fu Lai, Abel Laval, Laurane Marco, Christophe Petit",
       annee: "2023",
-      acceptedat: "IndoCrypt 2023",
+      acceptedat: "<a class='colored' href='https://crsind.in/indocrypt2023/'>IndoCrypt 2023</a>",
       
       liens: {
          "pdf": "https://eprint.iacr.org/2023/1710.pdf",
