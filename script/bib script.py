11aae352702d7c2cf0bcdd0242549ce32f32b8ba
abellaval <abel.laval@ulb.be>
2025-09-27 00:21:07 +0200

    updated bib

--- script/bib script.py
+++ script/bib script.py
@@ -3,22 +3,42 @@ import os
 
 bib_dir = './bib/'
 output_file = './bib.html'
-sections = ['KLPT & IKO', 'SQIsign', 'Isogenies','Mathematics', 'Drinfeld Modules','Cryptography', 'Books', 'Theses']
+sections = ['KLPT & IKO', 'SQIsign', 'Isogenies','Mathematics', 'Drinfeld Modules','Cryptography', 'Books', 'Theses', '"Science"']
 
 
-def import_pdf(section_name) :
+def import_pdf(section_name, split=True) :
 
     # Import files
     dir = bib_dir + section_name
     print(f"dir = {dir}")
     pdf_files = [f for f in os.listdir(dir) if f.lower().endswith('.pdf')]
 
+    # Titleless articles are put above the others
+    if split :
+        without_title = []
+        with_title = []
+
+        for filename in pdf_files:
+            if filename[-5] == ']' : without_title.append(filename)
+            else : with_title.append(filename)
+            
+        without_title.sort()
+        with_title.sort()
+        pdf_files = without_title + with_title
+        print(pdf_files)
+
     # Convert to html
     list_items = []
     for filename in pdf_files:
         name = os.path.splitext(filename)[0]
+
+
         list_items.append(f'\t\t<li><a class="bib" href="{dir}/{filename}" target="_blank">{name}</a></li>')
-    list_items.sort()
+
+    #list_items.sort()
+
+
+    
 
     # Generate the html section
     html_section = f"""
