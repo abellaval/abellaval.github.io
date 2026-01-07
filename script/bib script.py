1ac3000768f1cf0cc31efc1cc5294f5de78b1bca
abellaval <91144521+abellaval@users.noreply.github.com>
2026-01-07 16:38:54 +0100

    added bib categories and philosophy

--- script/bib script.py
+++ script/bib script.py
@@ -1,87 +1,91 @@
 import os
 
+def load_categories(bib_dir, output_file, sections) :
+    """
+    Load bib for a specific category : math, philo, econ...
+    """
 
-bib_dir = './bib/'
-output_file = './bib.html'
-sections = ['KLPT & IKO','SQIsign','Isogenies','Mathematics','Drinfeld Modules','Cryptography','Lattices','Books','Theses','Misc']
+    def import_pdf(section_name, split=True) :
+        # Import files
+        dir = bib_dir + section_name
+        pdf_files = [f for f in os.listdir(dir) if f.lower().endswith('.pdf')]
 
+        # Titleless articles are put above the others
+        if split :
+            without_title = []
+            with_title = []
 
-def import_pdf(section_name, split=True) :
+            for filename in pdf_files:
+                if filename[-5] == ']' : without_title.append(filename)
+                else : with_title.append(filename)
+                
+            without_title.sort()
+            with_title.sort()
+            pdf_files = without_title + with_title
+            #print(pdf_files)
 
-    # Import files
-    dir = bib_dir + section_name
-    pdf_files = [f for f in os.listdir(dir) if f.lower().endswith('.pdf')]
-
-    # Titleless articles are put above the others
-    if split :
-        without_title = []
-        with_title = []
-
+        # Convert to html
+        list_items = []
         for filename in pdf_files:
-            if filename[-5] == ']' : without_title.append(filename)
-            else : with_title.append(filename)
-            
-        without_title.sort()
-        with_title.sort()
-        pdf_files = without_title + with_title
-        #print(pdf_files)
+            name = os.path.splitext(filename)[0]
 
-    # Convert to html
-    list_items = []
-    for filename in pdf_files:
-        name = os.path.splitext(filename)[0]
+            list_items.append(f'\t\t<li><a class="bib" href="{dir}/{filename}" target="_blank">{name}</a></li>')
 
+        # Generate the html section
+        html_section = f"""
+        \n<!-- {section_name} -->
+        <h2 class='bib'>— {section_name} —</h2>
+        <ol>
+    {chr(10).join(list_items)}
+        </ol>\n"""
 
-        list_items.append(f'\t\t<li><a class="bib" href="{dir}/{filename}" target="_blank">{name}</a></li>')
-
-    #list_items.sort()
+        return html_section
 
 
-    
+    html_head = f"""<!DOCTYPE html>
+    <html lang="en">
+        <head>
+            <title>Abel Laval</title>
+            <link rel="icon" type="image/png" href="favicon.svg">
+            <link rel="stylesheet" href="css/style.css">
+            <meta charset="UTF-8">
+        </head>
 
-    # Generate the html section
-    html_section = f"""
-    \n<!-- {section_name} -->
-    <h2 class='bib'>— {section_name} —</h2>
-    <ol>
-{chr(10).join(list_items)}
-    </ol>\n"""
+    <body>
+        <p class="bib">
+            <a style="margin-right: 10px;"  href="index.html">home</a>
+        </p>"""
 
-    return html_section
-
-
-
-html_head = f"""<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <title>Abel Laval</title>
-        <link rel="icon" type="image/png" href="favicon.svg">
-        <link rel="stylesheet" href="css/style.css">
-        <meta charset="UTF-8">
-    </head>
-
-<body>
-    <p class="bib">
-        <a style="margin-right: 10px;"  href="index.html">home</a>
-    </p>"""
-
-html_coda = f"""</body>
-</html>"""
+    html_coda = f"""</body>
+    </html>"""
 
 
-# Head of the html page
-html_page = html_head
+    # Head of the html page
+    html_page = html_head
 
-# Add the sections
-for section in sections :
-    html_page += import_pdf(section)
+    # Add the sections
+    for section in sections :
+        html_page += import_pdf(section)
 
-# End of the html code
-html_page += html_coda
+    # End of the html code
+    html_page += html_coda
 
 
-# Write to bib.html
-with open(output_file, 'w', encoding='utf-8') as f:
-    f.write(html_page)
+    # Write to bib.html
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write(html_page)
 
-print(f"{output_file[2:]} has been successfully generated.")
+    print(f"---> {output_file[2:]} has been successfully generated.")
+
+
+
+
+# One bib page per topic
+categories = [
+    ['./bib/math/','./bib.html', ['KLPT & IKO','SQIsign','Mathematics','Drinfeld Modules','Cryptography','Lattices','Books','Theses','Misc']],
+    ['./bib/philo/', './philo.html', ['Philosophy of mind','Ethics']]
+]
+
+for category in categories :
+    bib_dir, output_file, sections = category
+    load_categories(bib_dir, output_file, sections)
